797e1d9348590b2fd762fb016ad82c4a96fac700
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-10 22:44:27 -0300

    att

--- trip/src/App.jsx
+++ trip/src/App.jsx
@@ -3,9 +3,16 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <img src="" alt="" />
+      <h1>plann.er</h1>
+      <p>Convide seus amigos e planeje sua próxima viagem!</p>
+    </div>
+    <div>
+      <input type="text" placeholder='Para onde você vai?'/>
+      <input type="text" placeholder='Quando?'/>
+      <button>Continuar</button>
+    </div>
     </>
   )
 }
