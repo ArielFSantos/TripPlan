43d4319d7e661f9f6ef3be1197805bd37f562357
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-10 22:46:02 -0300

    Update App.jsx

--- trip/src/App.jsx
+++ trip/src/App.jsx
@@ -13,6 +13,8 @@ function App() {
       <input type="text" placeholder='Quando?'/>
       <button>Continuar</button>
     </div>
+    <p>Ao planejar sua viagem pela plann.er você automaticamente concorda
+    com nossos <a href="">termos de uso</a> e <a href="">políticas de privacidade</a>.</p>
     </>
   )
 }
